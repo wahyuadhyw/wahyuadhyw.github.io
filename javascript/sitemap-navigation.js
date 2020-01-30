22135a4e49fa98bfad98aff43c7297bb81dfe3a5
Wahyu Adhy Wicaksono <wahyuadhyw@icloud.com>
2020-01-31 02:45:06 +0700

    Update sitemap-navigation.js

--- javascript/sitemap-navigation.js
+++ javascript/sitemap-navigation.js
@@ -17,20 +17,20 @@
 // Arlinadzgn
 // Panduanblog
 
-   var postTitle = new Array();     	 	 // array of posttitles
-   var postUrl = new Array();      	 	// array of posturls
-   var postDate = new Array();         // array of post publish dates
-   var postSum = new Array();        // array of post summaries
-   var postLabels = new Array();	// array of post labels
+   var postTitle = new Array(); // array of posttitles
+   var postUrl = new Array(); // array of posturls
+   var postDate = new Array(); // array of post publish dates
+   var postSum = new Array(); // array of post summaries
+   var postLabels = new Array(); // array of post labels
 
 // global variables
-   var sortBy = "datenewest";        							       // default value for sorting ToC
-   var tocLoaded = false;        									  // true if feed is read and ToC can be displayed
-   var numChars = 250;             								 // number of characters in post summary
-   var postFilter = '';            										// default filter value
-   var tocdiv = document.getElementById("bp_toc");   //the toc container
-   var totalEntires =0; 											  //Entries grabbed till now
-   var totalPosts =0; 												 //Total number of posts in the blog.
+   var sortBy = "datenewest"; // default value for sorting ToC
+   var tocLoaded = false; // true if feed is read and ToC can be displayed
+   var numChars = 250; // number of characters in post summary
+   var postFilter = ''; // default filter value
+   var tocdiv = document.getElementById("bp_toc"); //the toc container
+   var totalEntires =0; //Entries grabbed till now
+   var totalPosts =0; //Total number of posts in the blog.
 
 // main callback function
 
